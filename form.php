401471e017fa57365cf8f1298cd4ea1de9b72036
Nicola Mascolo <n.mascolopro@gmail.com>
2018-02-15 12:36:40 +0100

    adding temporary file

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+    <form action="adress.php" method="post">
+
+        <div class="form-group">
+            <input type="password" name="password" class="form-control" placeholder="Entrez votre mot de passe"
+                   value="544548">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Se connecter</button>
+    </form>
+
+<?php  ?>
