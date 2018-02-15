b278114501546f293ebe15b143a8915c1c729ba5
Nicola Mascolo <n.mascolopro@gmail.com>
2018-02-15 16:16:56 +0100

    completed form script

--- adress.php
+++ adress.php
@@ -13,22 +13,31 @@ function createAdress($streetnumber, $streetname, $postalcode, $city, $country){
 
     if (empty(($streetnumber) || ($streetname) || ($postalcode) || ($city) || ($country))) {
         $error = 'Vous devez remplir tous les champs du formulaire!';
+    } elseif (!empty( ($streetname) || ($city) || ($country))){
+        $arrayForm = array($streetname, $city, $country);
+        foreach($arrayForm as $elementForm){
+            if(ctype_digit($elementForm)){
+                $error = 'Vous ne pouvez pas pas utiliser de chiffres dans les cases "nom de rue", "nom de ville" et 
+                nom de pays"';
+            }
+        }
     }
 
     return $error;
 }
 
-if(empty($_POST['streetnumber'] || $_POST['streetname'] || $_POST['postalcode'] || $_POST['city'] || $_POST['country'])) {
+if(!empty($_POST['streetnumber'] || $_POST['streetname'] || $_POST['postalcode'] || $_POST['city'] || $_POST['country'])) {
     $message_error = createAdress($_POST['streetnumber'], $_POST['streetname'],$_POST['postalcode'], $_POST['city'], $_POST['country']);
 
     $_SESSION['message_error'] = $message_error;
     header('Location: form.php'); // changer selon la page du formulaire
-} elseif($_POST['streetnumber'] || $_POST['streetname'] || $_POST['postalcode'] || $_POST['city'] || $_POST['country']){
-    $message_error = createAdress($_POST['streetnumber'], $_POST['streetname'],$_POST['postalcode'], $_POST['city'], $_POST['country']);
+} elseif(empty($_POST['streetnumber'] || $_POST['streetname'] || $_POST['postalcode'] || $_POST['city'] || $_POST['country'])) {
+    $message_error = createAdress($_POST['streetnumber'], $_POST['streetname'], $_POST['postalcode'], $_POST['city'], $_POST['country']);
 
     $_SESSION['message_error'] = $message_error;
     header('Location: form.php'); // changer selon la page du formulaire
 }
+
 
 
 function lengthPassword($password){
@@ -46,14 +55,12 @@ function lengthPassword($password){
     return $error;
 }
 
-if(empty($_POST['password']) || $_POST['password']){
+if(!empty($_POST['password']) || $_POST['password']){
     $message_error = lengthPassword($_POST['password']);
 
     $_SESSION['message_error'] = $message_error;
     header('Location: form.php'); // changer selon la page du formulaire
-}
-
-if($_POST['password']){
+} elseif(empty($_POST['password']) || $_POST['password']) {
     $message_error = lengthPassword($_POST['password']);
 
     $_SESSION['message_error'] = $message_error;
--- form.php
+++ form.php
@@ -1,19 +1,47 @@
 <?php
 session_start();
 
-if($_SESSION['message_error'] == true){
-    echo $_SESSION['message_error'];
-};
+if(isset($_SESSION['message_error'])) {
+    $error = $_SESSION['message_error'];
+
+    unset($_SESSION['message_error']);
+}
 
 ?>
 
+<?php if(isset($error)): ?>
+    <div class="alert alert-warning" role="alert">
+        <?php echo $error ?>
+    </div>
+<?php endif; ?>
+
     <form action="adress.php" method="post">
+
+        <!-- <div class="form-group">
+            <input type="text" name="streetnumber" class="form-control" placeholder="Entrez le numéro de votre rue.">
+        </div>
+
+        <div class="form-group">
+            <input type="text" name="streetname" class="form-control" placeholder="Entrez le nom de votre rue.">
+        </div>
+
+        <div class="form-group">
+            <input type="text" name="postalcode" class="form-control" placeholder="Entrez votre code postal.">
+        </div>
+
+        <div class="form-group">
+            <input type="text" name="city" class="form-control" placeholder="Entrez votre ville.">
+        </div>
+
+        <div class="form-group">
+            <input type="text" name="country" class="form-control" placeholder="Entrez votre pays.">
+        </div> -->
 
         <div class="form-group">
             <input type="password" name="password" class="form-control" placeholder="Entrez votre mot de passe">
         </div>
 
-        <button type="submit" class="btn btn-primary">Se connecter</button>
+        <button type="submit" class="btn btn-primary">Compléter</button>
     </form>
 
 <?php  ?>
